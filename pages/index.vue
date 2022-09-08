bcd3e5e70a72dcb7ca851cff2a25f6d3d5943053
ben-desktop <ben.basten@gmail.com>
2022-09-08 09:53:27 -0400

    Reproduce file generation issue

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <Tutorial/>
+  <div v-html="htmlString"></div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  computed: {
+    htmlString: function () {
+      return "<a href=\"/relative/path/to/website\">Click here!</a>"
+    }
+  }
 }
 </script>
